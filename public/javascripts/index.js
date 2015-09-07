c126889eec9c90819ad08d71959bc733a6e7f839
Netra <amitmotgi@AMITs-MacBook-Pro-3.local>
2015-09-07 11:16:34 -0700

    initial version

---
+++ public/javascripts/index.js
@@ -0,0 +1,13 @@
+!function($){
+  var multiNews = function(){
+  };
+  multiNews.prototype = {
+    setUp : function(){
+      this.eventListeners();
+    },
+    eventListeners : function(){
+    }
+  };
+  var execute = new multiNews();
+  execute.setUp();
+}(window.jQuery);
